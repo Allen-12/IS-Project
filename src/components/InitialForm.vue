b6578ea2124f06a9f216d22169662bab731e6873
Allen-12 <allennjuguna@gmail.com>
2021-12-28 15:34:44 +0300

    Refactored to include forms for both cylindrical and rectangular tanks

--- src/components/InitialForm.vue
+++ src/components/InitialForm.vue
@@ -1,114 +1,42 @@
 <template>
-    <v-form
-    ref="form.v"
-    v-model="valid"
-    lazy-validation
-    >
-    <v-text-field
-      v-model="volume"
-      :counter="20"
-      :rules="volumeRules"
-      label="Volume (L)"
-      required
-      clearable
-    ></v-text-field>
+  <v-container fluid>
+      <v-select
+        v-model="tankType"
+        :items="tanks"
+        label="Select a tank type"
+        outlined
+        @change="setTankType"
+        >
+      </v-select>
 
-    <v-text-field
-      v-model="height"
-      :counter="20"
-      :rules="heightRules"
-      label="Height (cm)"
-      required
-      clearable
-    ></v-text-field>
-
-    <v-text-field
-      v-model="diameter"
-      :counter="20"
-      :rules="diameterRules"
-      label="Diameter (cm)"
-      required
-      clearable
-    ></v-text-field>
-
-    <v-btn
-      color="success"
-      class="mr-4"
-      @click="saveSetupData"
-    >
-      Complete Setup
-    </v-btn>
-
-    <v-btn
-      color="error"
-      class="mr-4"
-      @click="reset"
-    >
-      Reset
-    </v-btn>
-  </v-form>
+      <CylindricalForm v-if="tankType === 'Cylindrical'"></CylindricalForm>
+      <RectangularForm v-if="tankType === 'Rectangular'"></RectangularForm>
+  </v-container>
 </template>
 
 <script>
-import Localbase from 'localbase'
-import {v4 as uuidv4} from 'uuid';
-
-let db = new Localbase('tank_db')
+import CylindricalForm from '../components/CylindricalForm.vue'
+import RectangularForm from '../components/RectangularForm.vue'
 
 export default {
     name: "InitialForm",
+    components: {
+      CylindricalForm,
+      RectangularForm
+    },
     data: () => ({
       valid: true,
-      volume: '',
-      volumeRules: [
-        v => !!v || 'Volume is required',
-        v => (v && Number.isInteger(Number(v))) || 'Volume must be a number',
-        v => (v && v > 0) || 'Volume must be greater than 0',
+      tanks: [
+        'Cylindrical',
+        'Rectangular'
       ],
-      height: '',
-      heightRules: [
-        v => !!v || 'Height is required',
-        v => (v && Number.isInteger(Number(v))) || 'Height must be a number',
-        v => (v && v > 0) || 'Height must be greater than 0',
-      ],
-      diameter: '',
-      diameterRules: [
-        v => !!v || 'Diameter is required',
-        v => (v && Number.isInteger(Number(v))) || 'Diameter must be a number',
-        v => (v && v > 0) || 'Diameter must be greater than 0',
-      ],
+      tankType: '',
     }),
 
     methods: {
-      validate () 
+      setTankType()
       {
-        this.$refs.form.validate()
-      },
-      reset ()
-      {
-        this.$refs.form.reset()
-      },
-      resetValidation ()
-      {
-        this.$refs.form.resetValidation()
-      },
-      saveSetupData()
-      {
-          let setupData =
-          {
-              id: uuidv4(),
-              volume: Number(this.volume),
-              diameter: Number(this.diameter),
-              height: Number(this.height),
-              surfaceArea: this.volume / this.height
-          }
-          console.log(setupData)
-          db.collection('tank_data').add(setupData).then(response => {
-            this.added = response.success
-            console.log(this.added);
-          })
-          // console.log(data);
-          this.$router.push({name: 'Home'})
+          console.log(this.tankType)
       }
     },
 }
