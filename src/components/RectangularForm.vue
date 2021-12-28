95c4a55dc8a8c2aa85e483d9b9b4c5f30e1d1004
Allen-12 <allennjuguna@gmail.com>
2021-12-28 15:33:43 +0300

    Created form component to setup data for a rectangular tank

---
+++ src/components/RectangularForm.vue
@@ -0,0 +1,124 @@
+<template>
+    <v-form
+      ref="form.v"
+      v-model="valid"
+      lazy-validation
+      >
+      <v-text-field
+        v-model="volume"
+        :counter="20"
+        :rules="volumeRules"
+        label="Volume (L)"
+        required
+        clearable
+      ></v-text-field>
+
+      <v-text-field
+        v-model="length"
+        :counter="20"
+        :rules="lengthRules"
+        label="Length (cm)"
+        required
+        clearable
+      ></v-text-field>
+
+      <v-text-field
+        v-model="width"
+        :counter="20"
+        :rules="widthRules"
+        label="Width (cm)"
+        required
+        clearable
+      ></v-text-field>
+
+      <v-text-field
+        v-model="height"
+        :counter="20"
+        :rules="heightRules"
+        label="Height (cm)"
+        required
+        clearable
+      ></v-text-field>
+
+      <v-btn
+        color="success"
+        class="mr-4"
+        @click="saveSetupData"
+      >
+        Complete Setup
+      </v-btn>
+
+      <v-btn
+        color="error"
+        class="mr-4"
+        @click="reset"
+      >
+        Reset
+      </v-btn>
+    </v-form>
+</template>
+<script>
+export default {
+    name: 'RectangularForm',
+    data: () => ({
+      valid: true,
+      volume: '',
+      volumeRules: [
+        v => !!v || 'Volume is required',
+        v => (v && Number.isInteger(Number(v))) || 'Volume must be a number',
+        v => (v && v > 0) || 'Volume must be greater than 0',
+      ],
+      length: '',
+      lengthRules: [
+        v => !!v || 'Length is required',
+        v => (v && Number.isInteger(Number(v))) || 'Length must be a number',
+        v => (v && v > 0) || 'Length must be greater than 0',
+      ],
+      width: '',
+      widthRules: [
+        v => !!v || 'Width is required',
+        v => (v && Number.isInteger(Number(v))) || 'Width must be a number',
+        v => (v && v > 0) || 'Width must be greater than 0',
+      ],
+      height: '',
+      heightRules: [
+        v => !!v || 'Height is required',
+        v => (v && Number.isInteger(Number(v))) || 'Height must be a number',
+        v => (v && v > 0) || 'Height must be greater than 0',
+      ],
+    }),
+
+    methods: {
+      validate () 
+      {
+        this.$refs.form.validate()
+      },
+      reset ()
+      {
+        this.$refs.form.reset()
+      },
+      resetValidation ()
+      {
+        this.$refs.form.resetValidation()
+      },
+      saveSetupData()
+      {
+          let setupData =
+          {
+              id: uuidv4(),
+              volume: Number(this.volume),
+              length: Number(this.length),
+              width: Number(this.width),
+              height: Number(this.height),
+              tank_type: 'rectangular'
+          }
+          console.log(setupData)
+          db.collection('tank_data').add(setupData).then(response => {
+            this.added = response.success
+            console.log(this.added);
+          })
+          this.$router.push({name: 'Home'})
+      },
+    },
+}
+</script>
