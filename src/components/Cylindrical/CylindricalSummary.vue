e69eda57b2f1b6caf2e3f7d202f0ca4e0e0e7312
Allen-12 <allennjuguna@gmail.com>
2021-12-28 16:42:08 +0300

    Setup form and summary pages for cylindrical tanks

---
+++ src/components/Cylindrical/CylindricalSummary.vue
@@ -0,0 +1,84 @@
+<template>
+    <v-container fluid>
+        <div class="blue-grey lighten-1 text-h5  text-center mb-2" >
+            <span class="white--text">Tank: Cylindrical</span>
+        </div>
+      <v-row dense>
+        <v-col cols="6">
+           <v-card
+            color="blue lighten-1"
+            dark
+          >
+            <v-card-title class="text-h5 white--text">
+              Volume (L)
+            </v-card-title>
+
+            <v-card-subtitle class="text-h6 font-weight-bold white--text">{{ volume.toLocaleString({maximumFractionDigits:2}) }}</v-card-subtitle>
+          </v-card>
+        </v-col>
+        <v-col cols="6">
+           <v-card
+            color="blue lighten-1"
+            dark
+          >
+            <v-card-title class="text-h5 white--text">
+              Height (cm)
+            </v-card-title>
+
+            <v-card-subtitle class="text-h6 font-weight-bold white--text">{{ height.toLocaleString({maximumFractionDigits:2}) }}</v-card-subtitle>
+          </v-card>
+        </v-col>
+      </v-row>
+      <v-row dense>
+        <v-col cols="6">
+           <v-card
+            color="blue lighten-1"
+            dark
+            >
+            <v-card-title class="text-h5 white--text">
+              Diameter (cm)
+            </v-card-title>
+
+            <v-card-subtitle class="text-h6 font-weight-bold white--text">{{ diameter.toLocaleString({maximumFractionDigits:2}) }}</v-card-subtitle>
+          </v-card>
+        </v-col>
+        <v-col cols="6">
+           <v-card
+            color="blue lighten-1"
+            dark
+            >
+            <v-card-title class="text-h5 white--text">
+              Surface Area (cm&sup2;)
+            </v-card-title>
+
+            <v-card-subtitle class="text-h6 font-weight-bold white--text">{{ surfaceArea.toLocaleString({maximumFractionDigits:2}) }}</v-card-subtitle>
+          </v-card>
+        </v-col>
+      </v-row>
+  </v-container>
+</template>
+<script>
+import Localbase from 'localbase'
+
+let db = new Localbase('tank_db')
+
+export default{
+    name: 'CylindricalSummary',
+    data: () => ({
+        volume: 0,
+        height: 0,
+        diameter: 0,
+        surfaceArea: 0,
+        tankType: ''
+    }),
+    created: function() {
+        db.collection('tank_data').get().then(tank_data => {
+            this.volume = tank_data[0].volume
+            this.diameter = tank_data[0].diameter
+            this.height = tank_data[0].height
+            this.surfaceArea = tank_data[0].surfaceArea
+            this.tankType = tank_data[0].tank_type
+        })
+    }
+}
+</script>
