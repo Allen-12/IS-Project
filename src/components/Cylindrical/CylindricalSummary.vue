7b5d091f8544b8441adf3c96c2c6e0cf8ca0ac01
Allen-12 <allennjuguna@gmail.com>
2022-01-03 10:26:08 +0300

    Removed blue on cards with tank dimensions

--- src/components/Cylindrical/CylindricalSummary.vue
+++ src/components/Cylindrical/CylindricalSummary.vue
@@ -1,57 +1,45 @@
 <template>
     <v-container fluid>
-        <div class="blue-grey lighten-1 text-h5  text-center mb-2" >
+        <div class="blue darken-1 text-h5  text-center mb-2" >
             <span class="white--text">Tank: Cylindrical</span>
         </div>
       <v-row dense>
         <v-col cols="6">
-           <v-card
-            color="blue lighten-1"
-            dark
-          >
-            <v-card-title class="text-h5 white--text">
+           <v-card>
+            <v-card-title class="text-h5">
               Volume (L)
             </v-card-title>
 
-            <v-card-subtitle class="text-h6 font-weight-bold white--text">{{ volume.toLocaleString({maximumFractionDigits:2}) }}</v-card-subtitle>
+            <v-card-subtitle class="text-h6 font-weight-bold">{{ volume.toLocaleString({maximumFractionDigits:2}) }}</v-card-subtitle>
           </v-card>
         </v-col>
         <v-col cols="6">
-           <v-card
-            color="blue lighten-1"
-            dark
-          >
-            <v-card-title class="text-h5 white--text">
+           <v-card>
+            <v-card-title class="text-h5">
               Height (cm)
             </v-card-title>
 
-            <v-card-subtitle class="text-h6 font-weight-bold white--text">{{ height.toLocaleString({maximumFractionDigits:2}) }}</v-card-subtitle>
+            <v-card-subtitle class="text-h6 font-weight-bold">{{ height.toLocaleString({maximumFractionDigits:2}) }}</v-card-subtitle>
           </v-card>
         </v-col>
       </v-row>
       <v-row dense>
         <v-col cols="6">
-           <v-card
-            color="blue lighten-1"
-            dark
-            >
-            <v-card-title class="text-h5 white--text">
+           <v-card>
+            <v-card-title class="text-h5">
               Diameter (cm)
             </v-card-title>
 
-            <v-card-subtitle class="text-h6 font-weight-bold white--text">{{ diameter.toLocaleString({maximumFractionDigits:2}) }}</v-card-subtitle>
+            <v-card-subtitle class="text-h6 font-weight-bold">{{ diameter.toLocaleString({maximumFractionDigits:2}) }}</v-card-subtitle>
           </v-card>
         </v-col>
         <v-col cols="6">
-           <v-card
-            color="blue lighten-1"
-            dark
-            >
-            <v-card-title class="text-h5 white--text">
+           <v-card>
+            <v-card-title class="text-h5">
               Surface Area (cm&sup2;)
             </v-card-title>
 
-            <v-card-subtitle class="text-h6 font-weight-bold white--text">{{ surfaceArea.toLocaleString({maximumFractionDigits:2}) }}</v-card-subtitle>
+            <v-card-subtitle class="text-h6 font-weight-bold">{{ surfaceArea.toLocaleString() }}</v-card-subtitle>
           </v-card>
         </v-col>
       </v-row>
--- src/components/Rectangular/RectangularSummary.vue
+++ src/components/Rectangular/RectangularSummary.vue
@@ -1,57 +1,69 @@
 <template>
     <v-container fluid>
-        <div class="blue-grey lighten-1 text-h5  text-center mb-2">
+        <div class="blue lighten-1 text-h5  text-center mb-2">
             <span class="white--text">Tank: Rectangular</span>
         </div>
       <v-row dense> 
         <v-col cols="6">
-           <v-card
-            color="blue lighten-1"
-            dark
-          >
-            <v-card-title class="text-h5 white--text">
+           <v-card>
+            <v-card-title class="text-h5">
               Volume (L)
             </v-card-title>
 
-            <v-card-subtitle class="text-h6 font-weight-bold white--text">{{ volume.toLocaleString({maximumFractionDigits:2}) }}</v-card-subtitle>
+            <v-card-subtitle class="text-h6 font-weight-bold">{{ volume.toLocaleString({maximumFractionDigits:2}) }}</v-card-subtitle>
+            <!-- <v-card-actions>
+              <template>
+                <div class="text-center">
+                  <v-dialog
+                    v-model="dialog"
+                    width="500"
+                  >
+                    <v-tooltip left>
+                      <template v-slot:activator="{ on,attrs }">
+                        <v-btn 
+                          color="warning"
+                          v-bind="attrs"
+                          v-on="on"
+                        >
+                          <v-icon>mdi-pencil</v-icon>
+                        </v-btn>
+                      </template>
+                      <span>Edit</span>
+                    </v-tooltip>
+                  </v-dialog>
+                </div>
+
+              </template>
+            </v-card-actions> -->
           </v-card>
         </v-col>
         <v-col cols="6">
-           <v-card
-            color="blue lighten-1"
-            dark
-          >
-            <v-card-title class="text-h5 white--text">
+           <v-card>
+            <v-card-title class="text-h5">
               Length (cm)
             </v-card-title>
 
-            <v-card-subtitle class="text-h6 font-weight-bold white--text">{{ length.toLocaleString({maximumFractionDigits:2}) }}</v-card-subtitle>
+            <v-card-subtitle class="text-h6 font-weight-bold">{{ length.toLocaleString({maximumFractionDigits:2}) }}</v-card-subtitle>
           </v-card>
         </v-col>
       </v-row>
       <v-row dense>
         <v-col cols="6">
-           <v-card
-            color="blue lighten-1"
-            dark
-            >
-            <v-card-title class="text-h5 white--text">
+           <v-card>
+            <v-card-title class="text-h5">
               Width (cm)
             </v-card-title>
 
-            <v-card-subtitle class="text-h6 font-weight-bold white--text">{{ width.toLocaleString({maximumFractionDigits:2}) }}</v-card-subtitle>
+            <v-card-subtitle class="text-h6 font-weight-bold">{{ width.toLocaleString({maximumFractionDigits:2}) }}</v-card-subtitle>
           </v-card>
         </v-col>
         <v-col cols="6">
-           <v-card
-            color="blue lighten-1"
-            dark
-            >
-            <v-card-title class="text-h5 white--text">
+           <v-card>
+            <v-card-title class="text-h5">
               Height (cm)
             </v-card-title>
 
-            <v-card-subtitle class="text-h6 font-weight-bold white--text">{{ height.toLocaleString({maximumFractionDigits:2}) }}</v-card-subtitle>
+            <v-card-subtitle class="text-h6 font-weight-bold">{{ height.toLocaleString({maximumFractionDigits:2}) }}</v-card-subtitle>
           </v-card>
         </v-col>
       </v-row>
@@ -69,18 +81,35 @@ export default{
         length: 0,
         width: 0,
         height: 0,
-        tankType: ''
+        tankType: '',
+        dialog: false
     }),
     created: function() {
         db.collection('tank_data').get().then(tank_data => {
-            console.log(typeof tank_data[0].volume);
+            // console.log(typeof tank_data[0].volume);
             this.volume = tank_data[0].volume
             this.length = tank_data[0].length
             this.width = tank_data[0].width
             this.height = tank_data[0].height
             this.tankType = tank_data[0].tank_type
-            console.log(this.surfaceArea);
+            // console.log(this.surfaceArea);
         })
+    },
+    methods:{
+      // editValue(value){
+      //   switch (value) {
+      //     case "volume":
+      //       db.collection('users').doc({ tank_type: "rectangular" }).update({
+      //         volume: 'William'
+      //       })
+      //       break;
+        
+      //     default:
+      //       break;
+      //   }
+
+      //   // console.log(value);
+      // }
     }
 }
 </script>
